a77b93bc036f09b2c4d49c17487432ce54ade4c0
LCB2021029-Badri <lcb2021029@iiitl.ac.in>
2025-03-03 14:03:23 +0530

    simple stack navigation

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ import ScreenFlexBox from './ui/ScreenFlexBox';
 import MyScreen from './ui/MyScreen';
 import HomeScreen from './uiNeoPop/HomeScreen';
 import {name as appName} from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-AppRegistry.registerComponent(appName, () => HomeScreen);
+const Stack = createNativeStackNavigator();
+
+const Badri = () =>{
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="App" component={App} />
+                <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+                <Stack.Screen name="MyScreen" component={MyScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+AppRegistry.registerComponent(appName, () => Badri);
